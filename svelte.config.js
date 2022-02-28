fe793832c78544d3cc99ba3b763f9d746d958f89
jm61 <jean.moirano@gmail.com>
2022-02-28 23:11:02 +0100

    error in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@
 import adapter from '@sveltejs/adapter-netlify'
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
