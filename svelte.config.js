dc4f325c1c459b26f52160ed9051d55784ddbb78
jm61 <jean.moirano@gmail.com>
2022-02-28 23:44:52 +0100

    reverted to static for test github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Config} */
 //import adapter from '@sveltejs/adapter-auto'
-import adapter from '@sveltejs/adapter-netlify'
-//import adapter from '@sveltejs/adapter-static'
+//import adapter from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-static'
 const config = {
 
 	kit: {
